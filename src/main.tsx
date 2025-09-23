d5506b9dfb001cec6a9fe2266d33569b37b7ac1a
Phong Vu <caophong.w@gmail.com>
2025-09-24 00:06:45 +0700

    feat: initialize

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css'
+import App from './App.tsx'
+import AboutMe from './pages/AboutMe.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      // {
+      //   path: 'achievements',
+      //   element: <Achievements />,
+      // },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProps {
+}
+
+const AboutMe: React.FC<IProps> = () => {
+  return (
+    <div>
+        About me
+    </div>
+  )
+}
+export default AboutMe
