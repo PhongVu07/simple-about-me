885d0f5a8b11ad7f00a81a2bccb20360ac01ef2e
Phong Vu <caophong.w@gmail.com>
2025-09-24 21:56:23 +0700

    feat: Finalize AboutMe page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import About from '../components/About'
 
-interface IProps {
-}
-
-const AboutMe: React.FC<IProps> = () => {
+const AboutMe = () => {
   return (
-    <div>
-        About me
+    <div className="relative min-h-screen overflow-hidden text-slate-100">
+      <div className="animated-gradient-bg absolute inset-0 -z-10"></div>
+      <Hero />
+      <About />
     </div>
   )
 }
